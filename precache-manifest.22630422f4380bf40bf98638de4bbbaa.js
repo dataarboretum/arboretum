eb3e69ba59fdc246bb8b8de09a9fcc8d2f6fc23b
data.arboretum.llc <data.arboretum.llc@gmail.com>
2022-08-22 22:07:21 -0500

    Updates

---
+++ precache-manifest.22630422f4380bf40bf98638de4bbbaa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ae7eea1dc24dfa8843b4e52437a32fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "831c5634ee1b3459fd1e",
+    "url": "/static/css/2.80f479d8.chunk.css"
+  },
+  {
+    "revision": "e27b4a0ad448ff17e108",
+    "url": "/static/css/main.588a393e.chunk.css"
+  },
+  {
+    "revision": "831c5634ee1b3459fd1e",
+    "url": "/static/js/2.e36a4518.chunk.js"
+  },
+  {
+    "revision": "e27b4a0ad448ff17e108",
+    "url": "/static/js/main.e7d10ac2.chunk.js"
+  },
+  {
+    "revision": "0e0251a597dbff5db3d6",
+    "url": "/static/js/runtime-main.49ad4ca1.js"
+  }
+]);
